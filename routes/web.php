960f1efdeaec8d8b47398d2c28fae957f3fa99f2
qylinl <luna.vguinevere@gmail.com>
2021-03-02 09:57:31 +0700

    Laravel buku

---
+++ routes/web.php
@@ -0,0 +1,30 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/create/student', function(){
+    $student = \App\Student::create([
+        'nama' => 'Qylin',
+        'nis' => 13342,
+        'jenis_kelamin' => 'P',
+        'alamat' => 'tpl',
+        'email' => 'qylin@gmail.com',
+        'remember_token' => \Str::random(60)
+    ]);
+
+    return $student;
+});
+
